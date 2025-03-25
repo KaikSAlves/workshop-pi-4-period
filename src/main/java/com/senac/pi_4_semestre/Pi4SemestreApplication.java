14cf26a6ebd11df6e9cf634b1fb8a18c7e72610b
KAIK DE SOUZA ALVES <kaik.salves@senacsp.edu.br>
2025-03-25 08:46:06 -0300

    Config h2 database

---
+++ src/main/java/com/senac/pi_4_semestre/Pi4SemestreApplication.java
@@ -0,0 +1,13 @@
+package com.senac.pi_4_semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi4SemestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi4SemestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/pi_4_semestre/Pi4SemestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.pi_4_semestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi4SemestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
