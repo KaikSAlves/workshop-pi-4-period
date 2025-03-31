cbafd9fbf31df76c293ebe932f3e95c188e364b6
KAIK DE SOUZA ALVES <kaik.salves@senacsp.edu.br>
2025-03-31 08:51:27 -0300

    Abstract class Product

--- src/main/java/com/senac/pi_4_semestre/model/Product.java
+++ src/main/java/com/senac/pi_4_semestre/model/Product.java
@@ -3,6 +3,9 @@ package com.senac.pi_4_semestre.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.hibernate.annotations.Collate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,25 +13,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_product")
-public class Product implements Serializable{
+public abstract class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false, length = 150)
 	private String name;
-	private String description;
-	private Double price;
-	private String imgUrl;
-	
-	public Product() {
-		
-	}
 
-	public Product(Long id, String name, String description, Double price, String imgUrl) {
-		super();
+	@Column(nullable = false, length = 250)
+	private String description;
+
+	@Column(nullable = false)
+	private Double price;
+
+	
+	//TODO Tratar o tipo da imagem
+	private String imgUrl;
+
+	public Product(){
+	}
+	
+	public Product(String name, String description, Double price) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
@@ -39,57 +47,33 @@ public class Product implements Serializable{
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 	public Double getPrice() {
 		return price;
 	}
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
 	public String getImgUrl() {
 		return imgUrl;
 	}
-
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		return Objects.equals(id, other.id);
-	}
+	
 }
